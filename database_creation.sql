19a6575c243bd61ee1417ec90cbb4d13913bf5fa
davschne <davschne@gmail.com>
2015-10-29 01:26:57 -0700

    Database creation scripts

--- database_creation.sql
+++ database_creation.sql
@@ -1,8 +1,5 @@
-BEGIN;
-
 CREATE USER friending_library_user PASSWORD 'test';
+
 CREATE DATABASE friending_library
   OWNER friending_library_user
   TEMPLATE template0;
-
-COMMIT;
--- database_setup.sql
+++ database_setup.sql
@@ -5,16 +5,16 @@ CREATE TABLE Users (
   display_name varchar,
   given_name varchar,
   surname varchar,
-  PRIMARY KEY pk (uID)
+  PRIMARY KEY (uID)
 );
 
 CREATE INDEX UserNames ON Users (surname, given_name); -- index for user searches
 
-CREATE TABLE Friendship (
-  uID1 integer REFERENCES Users (uID) ON DELETE CASCADE,
-  uID2 integer REFERENCES Users (uID) ON DELETE CASCADE,
-  PRIMARY KEY pk (uID1, uID2)
-);
+-- CREATE TABLE Friendship (
+--   uID1 integer REFERENCES Users (uID) ON DELETE CASCADE,
+--   uID2 integer REFERENCES Users (uID) ON DELETE CASCADE,
+--   PRIMARY KEY (uID1, uID2)
+-- );
 
 CREATE TABLE Books (
   ISBN integer,
@@ -26,23 +26,14 @@ CREATE TABLE Books (
   pageCount integer,
   language varchar,
   imageLink varchar,
-  PRIMARY KEY pk (ISBN)
+  PRIMARY KEY (ISBN)
 );
-
--- create a materialized view to hold denormalized book data?
--- where would this view be used? where wouldn't it be used?
--- if multiple authors, combine in string as concatenated list?
--- possible to index a materialized view?
--- CREATE MATERIALIZED VIEW BookView AS
---   SELECT *
---   FROM (Authors JOIN Wrote USING(aID))
---     JOIN Books USING(ISBN);
 
 CREATE TABLE Authors (
   aID serial,
   surname varchar,
   given_name varchar,
-  PRIMARY KEY pk (aID)
+  PRIMARY KEY (aID)
 );
 
 CREATE INDEX AuthorNames ON Authors (surname, given_name); -- index for searches by author name
@@ -50,38 +41,48 @@ CREATE INDEX AuthorNames ON Authors (surname, given_name); -- index for searches
 CREATE TABLE Wrote (
   aID integer REFERENCES Authors ON DELETE CASCADE,
   ISBN integer REFERENCES Books ON DELETE CASCADE,
-  PRIMARY KEY pk (aID, ISBN)
+  PRIMARY KEY (aID, ISBN)
 );
+
+-- create a materialized view to hold denormalized book data?
+-- where would this view be used? where wouldn't it be used?
+-- if multiple authors, combine in string as concatenated list?
+-- possible to index a materialized view?
+CREATE MATERIALIZED VIEW BookView AS
+  SELECT *
+  FROM (Authors JOIN Wrote USING(aID))
+    JOIN Books USING(ISBN);
 
 CREATE TABLE Copies (
   copyID serial,
   ISBN integer REFERENCES Books ON DELETE CASCADE,
-  ownerID integer REFERENCES Users ON DELETE CASCADE,
-  PRIMARY KEY pk (ownerID, ISBN, copyID)
+  ownerID integer REFERENCES Users (uID) ON DELETE CASCADE,
+  PRIMARY KEY (copyID),
+  UNIQUE (ownerID, ISBN, copyID)
 );
 
 CREATE TABLE Borrowing (
-  borrowerID integer REFERENCES Users ON DELETE RESTRICT,
+  borrowerID integer REFERENCES Users (uID) ON DELETE RESTRICT,
   copyID integer REFERENCES Copies ON DELETE CASCADE,
   checkout_date TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY pk (borrowerID, copyID)
+  PRIMARY KEY (borrowerID, copyID)
 );
 
 CREATE TABLE BookRequests (
-  requesterID integer REFERENCES Users ON DELETE CASCADE,
+  requesterID integer REFERENCES Users (uID) ON DELETE CASCADE,
   copyID integer REFERENCES Copies ON DELETE CASCADE,
   request_date TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY pk (requesterID, copyID)
+  PRIMARY KEY (requesterID, copyID)
   -- optimized for requester's view ; possible to optimize for book owner?
   -- separate indexes on the columns?
 );
 
-CREATE TABLE FriendRequests (
-  requesterID integer REFERENCES Users ON DELETE CASCADE,
-  inviteeID integer REFERENCES Users ON DELETE CASCADE,
-  PRIMARY KEY pk (requesterID, inviteeID)
-  -- optimized for requester's view ; possible to optimize for invitee?
-  -- separate indexes on the columns?
-);
+-- CREATE TABLE FriendRequests (
+--   requesterID integer REFERENCES Users ON DELETE CASCADE,
+--   inviteeID integer REFERENCES Users ON DELETE CASCADE,
+--   PRIMARY KEY (requesterID, inviteeID)
+--   -- optimized for requester's view ; possible to optimize for invitee?
+--   -- separate indexes on the columns?
+-- );
 
 COMMIT;
