c3c74cfc04ea44ee43a770dcecf45f93ea0caf62
davschne <davschne@gmail.com>
2016-01-17 11:59:34 -0800

    closes #27: getLentBooks

--- test/test-db-util.js
+++ test/test-db-util.js
@@ -639,6 +639,74 @@ describe('db.js', function() {
     });
   });
 
+  describe("#getLentBooks", function() {
+
+    // users[0] is target for incoming book requests
+    var users = testData.users;
+    var books = testData.books;
+    var copies = [
+      { owner: users[0], book: books[0] },
+      { owner: users[0], book: books[1] },
+      { owner: users[0], book: books[2] }, // not in result set
+      { owner: users[1], book: books[3] }  // not in result set
+    ];
+
+    var borrowing = [
+      { borrower: users[0], copy: copies[3] }, // not in result set
+      { borrower: users[1], copy: copies[1] },
+      { borrower: users[2], copy: copies[0] }
+    ];
+
+    // setup: create users, books, copies, borrowing
+    before(function(done) {
+      // insert Users tuples (chain of Promises)
+      users.reduce(function(seq, user) {
+        return seq.then(insertUser.bind(null, db, user));
+      }, Promise.resolve())
+      // insert Books tuples
+      .then(books.reduce.bind(books, function(seq, book) {
+        return seq.then(insertBook.bind(null, db, book));
+      }, Promise.resolve()))
+      // insert Copies tuples
+      .then(copies.reduce.bind(copies, function(seq, copy, index) {
+        return seq.then(function() {
+          return insertCopy(db, copy.book, copy.owner);
+        })
+        .then(function(res) {
+          copies[index].copyid = res[0].copyid;
+        });
+      }, Promise.resolve()))
+      // insert Borrowing tuples
+      .then(borrowing.reduce.bind(borrowing, function(seq, borrowing) {
+        return seq.then(insertBorrowing.bind(null, db, borrowing.borrower, borrowing.copy.copyid));
+      }, Promise.resolve()))
+      .then(done.bind(null, null));
+    });
+
+    it("should return an array of tuples of the user's books currently being borrowed", function(done) {
+      db.getLentBooks(users[0].uid)
+      .then(function(res) {
+        expect(res).to.be.an.instanceof(Array);
+        expect(res).to.have.length(2);
+        expect(res[0].copyid).to.equal(borrowing[1].copy.copyid);
+        expect(res[0].borrowerid).to.equal(borrowing[1].borrower.uid.toString());
+        expect(res[0].isbn).to.equal(borrowing[1].copy.book.ISBN[13] || borrowing[1].copy.book.ISBN[10]);
+        expect(res[1].copyid).to.equal(borrowing[2].copy.copyid);
+        expect(res[1].borrowerid).to.equal(borrowing[2].borrower.uid.toString());
+        expect(res[1].isbn).to.equal(borrowing[2].copy.book.ISBN[13] || borrowing[2].copy.book.ISBN[10]);
+        done();
+      });
+    });
+
+    // cleanup: delete borrowing, users, books (copies will cascade)
+    after(function(done) {
+      deleteAllBorrowing(db)
+      .then(deleteAllUsers.bind(null, db))
+      .then(deleteAllBooks.bind(null, db))
+      .then(done.bind(null, null));
+    });
+  });
+
   // close connection to database
   after(function() {
     db.disconnect();
