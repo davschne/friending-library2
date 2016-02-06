20dfea59b64be825713e4a2415c0de452538e259
davschne <davschne@gmail.com>
2016-02-05 16:38:00 -0800

    altered REST service so that API calls return Promises to caller

--- client/src/shared/REST.js
+++ client/src/shared/REST.js
@@ -6,135 +6,131 @@ module.exports = function(friendingLibrary) {
     "REST",
     ["$http", "Token", "LoginLogout", function($http, Token, Log) {
 
-      function APICall(obj, callback) {
+      var checkFor401 = function(err, status) {
+        if (status === 401) {
+          // API call unauthorized because of invalid token
+          // Logout
+          Log.out();
+          return;
+        }
+        // otherwise delegate error handling to caller
+        else throw err;
+      };
+
+      var APICall = function(obj) {
         /*
         @param :  obj = {
                     url: <string>
                     method: <string>
                     [data: <string>]
                   }
-        @param :  callback <function>
         */
-        $http({
+        return $http({
           method: obj.method,
           url: obj.url,
           headers: {"Authorization": "Bearer " + Token.get()},
           data: obj.data || {}
         })
-        .success(function(data, status, headers, config) {
-          callback(null, data);
-        })
-        .error(function(data, status, headers, config) {
-          handle(status, callback);
-        });
-      }
-
-      function handle(status, callback) {
-        switch (status) {
-          // API call unauthorized because of invalid token
-          case 401:
-            Log.out();
-            break;
-          default:
-            callback({ status: status }, null);
-        }
-      }
+        .error(checkFor401);
+      };
 
       return {
 
-        deleteUser: function(callback) {
-          APICall({ method: "DELETE", url: "/api/self" }, callback);
+        deleteUser: function() {
+          return APICall({ method: "DELETE", url: "/api/self" });
         },
 
-        getIncomingBookRequests: function(callback) {
-          APICall({
+        getIncomingBookRequests: function() {
+          return APICall({
             method: "GET", url: "/api/self/book_requests/incoming"
-          }, callback);
+          });
         },
 
-        getOutgoingBookRequests: function(callback) {
-          APICall({
+        getOutgoingBookRequests: function() {
+          return APICall({
             method: "GET", url: "/api/self/book_requests/outgoing"
-          }, callback);
+          });
         },
 
-        getBorrowedBooks: function(callback) {
-          APICall({
+        getBorrowedBooks: function() {
+          return APICall({
             method: "GET", url: "/api/self/books_borrowed"
-          }, callback);
+          });
         },
 
-        getLentBooks: function(callback) {
-          APICall({
+        getLentBooks: function() {
+          return APICall({
             method: "GET", url: "/api/self/books_lent"
-          }, callback);
+          });
         },
 
-        getOwnBooks: function(callback) {
-          APICall({
+        getOwnBooks: function() {
+          return APICall({
             method: "GET", url: "/api/self/books"
-          }, callback);
+          });
         },
 
-        createCopy: function(data, callback) {
-          APICall({
+        createCopy: function(data) {
+          return APICall({
             method: "POST", url: "/api/books", data: data
-          }, callback);
+          });
         },
 
-        deleteCopy: function(copyid, callback) {
-          APICall({
+        deleteCopy: function(copyid) {
+          return APICall({
             method: "DELETE", url: "/api/books/" + copyid
-          }, callback);
+          });
         },
 
-        getAvailableBooks: function(callback) {
-          APICall({
+        getAvailableBooks: function() {
+          return APICall({
             method: "GET", url: "/api/books/available"
-          }, callback);
+          });
         },
 
-        createBookRequest: function(copyid, callback) {
-          APICall({
+        createBookRequest: function(copyid) {
+          return APICall({
             method: "POST", url: "/api/trans/request",
             data: { copyid: copyid }
-          }, callback);
+          });
         },
 
-        cancelBookRequest: function(copyid, callback) {
-          APICall({
+        cancelBookRequest: function(copyid) {
+          return APICall({
             method: "DELETE", url: "/api/trans/request/" + copyid
-          }, callback);
+          });
         },
 
-        checkoutBook: function(copyid, requesterid, callback) {
-          APICall({
+        checkoutBook: function(copyid, requesterid) {
+          return APICall({
             method: "POST",
             url: "/api/trans/checkout",
             data: { copyid: copyid, requesterid: requesterid }
-          }, callback);
+          });
         },
 
-        denyBookRequest: function(copyid, requesterid, callback) {
-          APICall({
+        denyBookRequest: function(copyid, requesterid) {
+          return APICall({
             method: "POST",
             url: "/api/trans/deny",
             data: { copyid: copyid, requesterid: requesterid }
-          }, callback);
+          });
         },
 
-        checkinBook: function(copyid, callback) {
-          APICall({
+        checkinBook: function(copyid) {
+          return APICall({
             method: "POST", url: "/api/trans/checkin", data: { copyid: copyid }
-          }, callback);
+          });
         },
 
-        queryGoogleBooks: function(isbn, callback) {
-          APICall({
-            method: "GET",
-            url: "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn + "&key=AIzaSyCDBfooq1pwrKzZzyUiBTa-cXHA25E63M0"
-          }, callback);
-        }
+        // TODO : Move this function to its own service
+
+        // queryGoogleBooks: function(isbn) {
+        //   APICall({
+        //     method: "GET",
+        //     url: "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn + "&key=AIzaSyCDBfooq1pwrKzZzyUiBTa-cXHA25E63M0"
+        //   });
+        // }
       };
     }]
   );
