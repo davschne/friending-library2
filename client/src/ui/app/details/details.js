7410ca4863e592d1a025eda3a5344818521cb2f8
davschne <davschne@gmail.com>
2016-03-31 20:35:44 -0700

    styling copy details modal

--- client/src/ui/app/details/details.js
+++ client/src/ui/app/details/details.js
@@ -5,7 +5,6 @@ module.exports = function(friendingLibrary) {
   friendingLibrary.controller(
     "detailsController", ["$scope", "$state", "$stateParams", function($scope, $state, $stateParams) {
 
-      console.log($stateParams);
       $scope.r    = $stateParams.record;
 
       // hide view by navigating back to parent state
